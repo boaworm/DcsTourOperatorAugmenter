3a8a5aea4cbe48b05a6a537a585e591e73ac9a30
boaworm <boaworm@gmail.com>
2014-10-04 21:40:44 +0000

    test

--- ab-dcs-touroperator-client/src/main/de/airberlin/ix/touroperator/DcsTourOperatorAugmenter.java
+++ ab-dcs-touroperator-client/src/main/de/airberlin/ix/touroperator/DcsTourOperatorAugmenter.java
@@ -1,4 +1,12 @@
 package de.airberlin.ix.touroperator;
 
+import com.sabre.ix.client.DcsFlightLeg;
+
 public interface DcsTourOperatorAugmenter {
+
+    /**
+     * Gather information from linked Booking objects into the DcsFlightLeg
+     * */
+    void augment(DcsFlightLeg dcsFlightLeg);
+
 }
--- ab-dcs-touroperator-client/src/main/de/airberlin/ix/touroperator/DcsTourOperatorAugmenterImpl.java
+++ ab-dcs-touroperator-client/src/main/de/airberlin/ix/touroperator/DcsTourOperatorAugmenterImpl.java
@@ -1,4 +1,11 @@
 package de.airberlin.ix.touroperator;
 
+import com.sabre.ix.client.DcsFlightLeg;
+
 public class DcsTourOperatorAugmenterImpl implements DcsTourOperatorAugmenter {
+
+    @Override
+    public void augment(DcsFlightLeg dcsFlightLeg) {
+
+    }
 }
